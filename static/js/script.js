33e209db4ac2e072a37455df7b86854297778e22
Kristopp <kristo.palu89@gmail.com>
2021-05-01 18:00:26 +0300

    removeCards

--- static/js/script.js
+++ static/js/script.js
@@ -8,22 +8,39 @@ let blackJackGame = {
   },
 };
 const YOU = blackJackGame["you"];
-const DEALER = blackJackGame["you"];
+const DEALER = blackJackGame["dealer"];
 
 document
   .querySelector("#blackJack-hit-button")
   .addEventListener("click", blackJackHit);
 
+document
+  .querySelector("#blackJack-deal-button")
+  .addEventListener("click", blackjackDeal);
+
 const hitSound = new Audio("static/sounds/swish.m4a");
 
 function blackJackHit() {
-  showCard();
+  showCard(YOU);
+  showCard(DEALER);
 }
 
-function showCard() {
+function showCard(activePlayer) {
   let cardImage = document.createElement("img");
   console.log(cardImage);
   cardImage.src = "/static/images/Q.png";
-  document.querySelector(YOU["div"]).appendChild(cardImage);
+  document.querySelector(activePlayer["div"]).appendChild(cardImage);
   hitSound.play();
+}
+
+function blackjackDeal() {
+  let yourImages = document.querySelector("#your-box").querySelectorAll("img");
+  let dealerImages = document.querySelector("#dealer-box").querySelectorAll("img");
+
+  for (let i = 0; i < yourImages.length; i++) {
+    yourImages[i].remove();
+  }
+  for (let i = 0; i < yourImages.length; i++) {
+    dealerImages[i].remove();
+  }
 }
