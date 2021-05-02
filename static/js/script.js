cee67333b198d918497abe41a80a657d9a356aca
Kristopp <kristo.palu89@gmail.com>
2021-05-02 19:56:54 +0300

    create random cards and add a value tgo each card

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,5 @@
 //Document selectors
+//General game Data
 let blackJackGame = {
   you: { scoreSpan: "#player-blackjack-result", div: "#your-box", score: 0 },
   dealer: {
@@ -6,10 +7,27 @@ let blackJackGame = {
     div: "#dealer-box",
     score: 0,
   },
+  /* Array of card names so we could use them later in the template string */
+  cards: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+  cardsMap: {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10,
+    "A": 10,
+  },
 };
 const YOU = blackJackGame["you"];
 const DEALER = blackJackGame["dealer"];
-
+//Click event listeners
 document
   .querySelector("#blackJack-hit-button")
   .addEventListener("click", blackJackHit);
@@ -17,30 +35,45 @@ document
 document
   .querySelector("#blackJack-deal-button")
   .addEventListener("click", blackjackDeal);
-
-const hitSound = new Audio("static/sounds/swish.m4a");
-
+//Hit button function
 function blackJackHit() {
-  showCard(YOU);
-  showCard(DEALER);
+  let card = randomCard();
+  showCard(YOU, card);
 }
-
-function showCard(activePlayer) {
+//Helper function that creates randomCard
+function randomCard() {
+  let randomIndex = Math.floor(Math.random() * 13);
+  return blackJackGame.cards[randomIndex];
+}
+//function that renders card
+function showCard(activePlayer, card) {
   let cardImage = document.createElement("img");
-  console.log(cardImage);
-  cardImage.src = "/static/images/Q.png";
+  cardImage.src = `static/images/${card}.png`;
   document.querySelector(activePlayer["div"]).appendChild(cardImage);
-  hitSound.play();
 }
-
+//Deal button removes all cards and creates new cards
 function blackjackDeal() {
   let yourImages = document.querySelector("#your-box").querySelectorAll("img");
-  let dealerImages = document.querySelector("#dealer-box").querySelectorAll("img");
-
+  let dealerImages = document
+    .querySelector("#dealer-box")
+    .querySelectorAll("img");
   for (let i = 0; i < yourImages.length; i++) {
     yourImages[i].remove();
   }
-  for (let i = 0; i < yourImages.length; i++) {
+  for (let i = 0; i < dealerImages.length; i++) {
     dealerImages[i].remove();
   }
 }
+
+function upDateScore(activePlayer, card) {
+
+  //we increment active player score to value of given card in our card map
+  activePlayer['score'] += blackJackGame['cardsMap'][card];
+/*   for (let key in blackJackGame.cardsMap) {
+    if (blackJackGame.cardsMap.hasOwnProperty(key)) {
+      console.log(`${key} : ${blackJackGame.cardsMap[key]}`);
+    }
+  } */
+}
+
+upDateScore();
